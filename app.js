40de8122a52c947f39b569a8b2f55f4731fde244
Angel Baez <angi020101@gmail.com>
2024-07-17 19:57:46 -0400

    segunda prueba

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("hola");
+let uni = "utic";
+console.log(typeof (uni));
+let edad = 45;
+console.log(typeof (edad));
+let pi = 3.14;
+console.log(typeof (pi));
+let soymillonario = false;
+console.log(typeof (soymillonario));
+let arreglo = [
+    uni, edad
+    , pi, soymillonario
+];
+const noBasta = "Si basta, el vasto, con el pasto";
+console.log(typeof (soymillonario));
+console.log(arreglo);
+console.log(arreglo.length);
+for (let i = 0; i < arreglo.length; i++) {
+    debugger;
+    console.log(arreglo[i]);
+}
